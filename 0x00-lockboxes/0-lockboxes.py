e83089a1453624b74088e1a5d2fad00ec589df01
santiagopemo <santiagopemo@gmail.com>
2020-12-08 21:37:39 -0500

    corrected pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Lock Boxes"""
-     
+
 
 def canUnlockAll(boxes):
     """Method that determines if all the boxes can be opened"""
@@ -10,13 +10,13 @@ def canUnlockAll(boxes):
         for i, keys in enumerate(boxes):
             if i in unic_keys:
                 for key in keys:
-                    if key not in unic_keys and ( 0 <= key < len(boxes)):
+                    if key not in unic_keys and (0 <= key < len(boxes)):
                         if i != 0 and key != 0:
                             new_key_added = True
                         unic_keys.append(key)
-        if new_key_added == False:
+        if new_key_added is False:
             break
     if len(unic_keys) == len(boxes):
         return True
     else:
-        return False  
+        return False
