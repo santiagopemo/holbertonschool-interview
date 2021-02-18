c6450b1695abe817a7c3ddc5a2c088830befd71e
santiagopemo <santiagopemo@gmail.com>
2021-02-18 14:05:24 -0500

    corrected 0 validation

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -7,12 +7,23 @@ def validUTF8(data):
     Method that determines if a given data
     set represents a valid UTF-8 encoding
     """
+    continuation = False
     for number in data:
-        b_index = bin(number).find("b")
-        data_byte = bin(number)[b_index + 1:]
-        if (len(data_byte) > 8):
-            data_byte = data_byte[-8:]
-        length = data_byte.find("0")
-        if (length > 4 or length <= 0):
+        data_byte = bin(number).replace("0b", "").rjust(8, "0")
+        if data_byte == "00000000":
             return False
+        if continuation is False:
+            if data_byte[0] == "0":
+                continue
+            char_length = data_byte.find("0")
+            if char_length > 4 or char_length == -1:
+                return False
+            if char_length > 1:
+                continuation = True
+        else:
+            if data_byte[0: 2] != "10":
+                return False
+            char_length -= 1
+            if char_length == 0:
+                continuation = False
     return True
