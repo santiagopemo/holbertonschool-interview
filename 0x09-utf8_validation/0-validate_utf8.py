4ce588947a5e7da950a0c62a6a9e4c128af204bc
santiagopemo <santiagopemo@gmail.com>
2021-02-18 11:45:35 -0500

    added utf8 validation project

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Validate UTF-8 module"""
+
+
+def validUTF8(data):
+    """
+    Method that determines if a given data
+    set represents a valid UTF-8 encoding
+    """
+    print(data)
+    for number in data:
+        b_index = bin(number).find("b")
+        data_byte = bin(number)[b_index + 1:]
+        if (len(data_byte) > 8):
+            data_byte = data_byte[-8:]
+        length = data_byte.find("0")
+        if (length > 4 or length == -1):
+            return False
+    return True
