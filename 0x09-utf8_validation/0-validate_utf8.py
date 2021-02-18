9f4e47b53a86e5e4ac9834918b0c755fad2b87fb
santiagopemo <santiagopemo@gmail.com>
2021-02-18 14:08:04 -0500

    corrected 0 validation

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -10,6 +10,8 @@ def validUTF8(data):
     continuation = False
     for number in data:
         data_byte = bin(number).replace("0b", "").rjust(8, "0")
+        if len(data_byte) > 8:
+            data_byte = data_byte[-8:]
         if data_byte == "00000000":
             return False
         if continuation is False:
