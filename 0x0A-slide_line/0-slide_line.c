b4ff281bf39836f91b04ae19af915496d44a793e
santiagopemo <santiagopemo@gmail.com>
2021-02-25 13:20:32 -0500

    corrected Betty style

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,6 +1,66 @@
 #include "slide_line.h"
 
 /**
+ * slide_left - slides and merges an array of integers to the left
+ * @line: points to an array of integers
+ * @size: line array elements
+ */
+void slide_left(int *line, size_t size)
+{
+	int i, j;
+
+	for (i = 0; i < (int) size; i++)
+	{
+		for (j = i + 1; j < (int) size; j++)
+		{
+			if (line[i] == 0 && line[j] > 0)
+			{
+				line[i] += line[j];
+				line[j] = 0;
+				i -= 1;
+				break;
+			}
+			if (line[j] > 0 && line[i] == line[j])
+			{
+				line[i] += line[j];
+				line[j] = 0;
+				break;
+			}
+		}
+	}
+}
+
+/**
+ * slide_right - slides and merges an array of integers to the right
+ * @line: points to an array of integers
+ * @size: line array elements
+ */
+void slide_right(int *line, size_t size)
+{
+	int i, j;
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		for (j = i - 1; j >= 0; j--)
+		{
+			if (line[i] == 0 && line[j] > 0)
+			{
+				line[i] += line[j];
+				line[j] = 0;
+				i += 1;
+				break;
+			}
+			if (line[j] > 0 && line[i] == line[j])
+			{
+				line[i] += line[j];
+				line[j] = 0;
+				break;
+			}
+		}
+	}
+}
+
+/**
  * slide_line - function that slides and merges an array of integers
  * @line: points to an array of integers
  * @size: line array elements
@@ -9,53 +69,15 @@
  */
 int slide_line(int *line, size_t size, int direction)
 {
-	int i, j;
-
 	if (direction == SLIDE_LEFT)
 	{
-		for (i = 0; i < (int) size; i++)
-		{
-			for (j = i + 1; j < (int) size; j++)
-			{
-				if (line[i] == 0 && line[j] > 0)
-				{
-					line[i] += line[j];
-					line[j] = 0;
-					i -= 1;
-					break;
-				}
-				if (line[j] > 0 && line[i] == line[j])
-				{
-					line[i] += line[j];
-					line[j] = 0;
-					break;
-				}
-			}
-		}
+		slide_left(line, size);
 		return (1);
 	}
 	if (direction == SLIDE_RIGHT)
 	{
-		for (i = size - 1; i >= 0; i--)
-		{
-			for (j = i - 1; j >= 0; j--)
-			{
-				if (line[i] == 0 && line[j] > 0)
-				{
-					line[i] += line[j];
-					line[j] = 0;
-					i += 1;
-					break;
-				}
-				if (line[j] > 0 && line[i] == line[j])
-				{
-					line[i] += line[j];
-					line[j] = 0;
-					break;
-				}
-			}
-		}
+		slide_right(line, size);
 		return (1);
-	}	
+	}
 	return (0);
 }
