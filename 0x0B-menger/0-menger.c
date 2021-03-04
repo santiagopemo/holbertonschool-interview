72451292d08deb9ad4cf74e08cabdabda839a854
santiagopemo <santiagopemo@gmail.com>
2021-03-04 17:01:11 -0500

    added menger sponge project

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,29 @@
+#include "menger.h"
+
+/**
+ * menger - function that draws a 2D Menger Sponge
+ * @level: the level of the Menger Sponge to draw
+ */
+void menger(int level)
+{
+	int i, j, p, ti, tj;
+
+	p = pow(3, level);
+	for (i = 0; i < p; i++)
+	{
+		for (j = 0; j < p; j++)
+		{
+			for (ti = i, tj = j; ti > 0 || tj > 0; ti /= 3, tj /= 3)
+			{
+				if (ti % 3 == 1 && tj % 3 == 1)
+				{
+					putchar(' ');	
+					break;
+				}					
+			}
+			if (ti == 0 || tj == 0)
+				putchar('#');
+		}
+		putchar('\n');
+	}
+}
---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,9 @@
+#ifndef _MERGER_H_
+#define _MERGER_H_
+
+#include <stdio.h>
+#include <math.h>
+
+void menger(int level);
+
+#endif
