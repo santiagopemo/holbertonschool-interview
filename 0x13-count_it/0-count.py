1d247ccc7f97fc24e569a0bd9bedfe115089f3e2
santiagopemo <santiagopemo@gmail.com>
2021-05-06 19:41:22 -0500

    added count it project

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""0-count.py"""
+import requests
+
+
+def count_words(subreddit, word_list, after='', words_dict={}):
+    """
+    Recursive function that queries the Reddit API, parses
+    the title of all hot articles, and prints a sorted count
+    of given keywords (case-insensitive, delimited by spaces
+    """
+    if words_dict == {}:
+        words_dict = dict.fromkeys(word_list, 0)
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    params = {'after': after}
+    headers = {'User-Agent': 'santiagopemo'}
+    response = requests.get(url, headers=headers, params=params,
+                                            allow_redirects=False)
+    if response.status_code != 200:
+        return
+    response_json = response.json()
+    data = response_json.get('data')
+    if data is None:
+        return
+    hot_list = data.get('children')
+    if hot_list is None:
+        return
+    for post in hot_list:
+        title = post.get('data').get('title').lower().split()
+        for word in word_list:
+            words_dict[word] += title.count(word.lower())
+    after = response_json.get('data').get('after')
+    if after is None:
+        sort_value = sorted(words_dict.items(), key=lambda i: i[::-1])
+        sort_alpha = sorted(sort_value, key=lambda i: i[1], reverse=True)
+        for word, value in sort_alpha:
+            if value != 0:
+                print(f'{word}: {value}')
+        return
+    count_words(subreddit, word_list, after, words_dict)
---
+++ 0x13-count_it/README.md
@@ -0,0 +1,23 @@
+# 0x13. Count it!
+## Resources :books:
+### Read or watch:
+
+* [Reddit API Documentation]()
+## Requirements :white_check_mark:
+### General
+* Allowed editors: vi, vim, emacs
+* All your files will be interpreted/compiled on Ubuntu 14.04 LTS using python3 (version 3.4.3)
+* All your files should end with a new line
+* The first line of all your files should be exactly #!/usr/bin/python3
+* Libraries imported in your Python files must be organized in alphabetical order
+* A README.md file, at the root of the folder of the project, is mandatory
+* Your code should use the PEP 8 style
+* All your files must be executable
+* The length of your files will be tested using wc
+* All your modules should have a documentation (python3 -c 'print(__import__("my_module").__doc__)')
+* You must use the Requests module for sending HTTP requests to the Reddit API
+## Tasks
+### Mandatory :page_with_curl:
+- [x] **[0. Count it!](./0-count.py)** - Write a recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords (case-insensitive, delimited by spaces. Javascript should count as javascript, but java should not)
+## Author :pencil:
+**Santiago Peña Mosquera** - twitter [@santiagopemo](https://twitter.com/santiagopemo) - LinkedIn [Santiago Peña Mosquera](https://www.linkedin.com/in/santiago-pe%C3%B1a-mosquera-abaa20196/)
