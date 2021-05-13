d22970e531dc8f875b5a148948abb189bf7a11b6
santiagopemo <santiagopemo@gmail.com>
2021-05-13 17:04:23 -0500

    added heap extract project

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,85 @@
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
+int heap_height(heap_t *root)
+{
+	int left, right;
+
+	if (root == NULL)
+		return (0);
+	left = heap_height(root->left);
+	right = heap_height(root->right);
+	return (left > right ? left + 1 : right + 1);	
+}
+heap_t *last_node(heap_t *root, int heigth)
+{
+	heap_t *left, *right;
+
+	if (root == NULL)
+		return (NULL);
+	if (heigth == 1)
+	{
+		/*printf("%d ", root->n);*/
+		return root;
+	}		
+	left = last_node(root->left, heigth - 1);
+	right = last_node(root->right, heigth - 1);
+	return (right == NULL ? left : right);	
+}
+/**
+ * heap_extract - Extracts the root node of a Max Binary Heap
+ * @root: heap's root node
+ * Return: the value of extracted node on success, 0 if not
+ */
+int heap_extract(heap_t **root)
+{
+	int extracted_value = (*root)->n;
+	int height = heap_height(*root);
+	heap_t *node = last_node(*root, height);
+	/*printf("\n%d\n", height);*/
+	if (node->parent->left == node)
+		node->parent->left = NULL;
+	else
+		node->parent->right = NULL;
+	(*root)->n = node->n;
+	free(node);
+	for(node = *root; node != NULL;)
+	{
+		if (node->right && node->left)
+		{
+			if(node->right->n > node->left->n && node->right->n > node->n)
+			{
+				swap(&node->right->n, &node->n);
+				node = node->right;
+			}
+			else if(node->left->n > node->right->n && node->left->n > node->n)
+			{
+				swap(&node->left->n, &node->n);
+				node = node->left;
+			}
+		}
+		else if (node->right && node->right->n > node->n)
+		{
+			swap(&node->right->n, &node->n);
+			node = node->right;
+		}
+		else if (node->left && node->left->n > node->n)
+		{
+			swap(&node->left->n, &node->n);
+			node = node->left;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return(extracted_value);
+}
---
+++ 0x14-heap_extract/README.md
@@ -0,0 +1,25 @@
+# 0x14. Heap Extract
+## Resources :books:
+* [None]()
+
+## Requirements :white_check_mark:
+### General
+* Allowed editors: vi, vim, emacs
+* All your files will be compiled on Ubuntu 14.04 LTS
+* Your programs and functions will be compiled with gcc 4.8.4 using the flags -Wall -Werror -Wextra and -pedantic
+* All your files should end with a new line
+* A README.md file, at the root of the folder of the project, is mandatory
+* Your code should use the Betty style. It will be checked using betty-style.pl and betty-doc.pl
+* You are not allowed to use global variables
+* No more than 5 functions per file
+* You are allowed to use the standard library
+* In the following examples, the main.c files are shown as examples. You can use them to test your functions, but you don’t have to push them to your repo (if you do we won’t take them into account). We will use our own main.c files at compilation. Our main.c files might be different from the one shown in the examples
+* The prototypes of all your functions should be included in your header file called binary_trees.h
+* Don’t forget to push your header file
+* All your header files should be include guarded
+
+## Tasks
+### Mandatory :page_with_curl:
+- [x] **[0. Heap - Extract](./0-heap_extract.c)** - Write a function that extracts the root node of a Max Binary Heap
+## Author :pencil:
+**Santiago Peña Mosquera** - twitter [@santiagopemo](https://twitter.com/santiagopemo) - LinkedIn [Santiago Peña Mosquera](https://www.linkedin.com/in/santiago-pe%C3%B1a-mosquera-abaa20196/)
