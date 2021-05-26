9826ce8375be99332dafd4f4ec2cd4b4f411c47f
santiagopemo <santiagopemo@gmail.com>
2021-05-26 14:30:25 -0500

    corrected pep8

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,18 +4,15 @@
 
 def rotate_2d_matrix(matrix):
     """Rotate a matrix 90 degrees clockwise"""
-    new_matrix = matrix.copy()
-    rows_n = len(new_matrix)
-    i = 0    
+    tmp_matrix = matrix.copy()
     matrix.clear()
+    rows_n = len(tmp_matrix)
+    i = 0
     while (i < rows_n):
         new_row = []
         j = rows_n - 1
         while (j >= 0):
-            new_row.append(new_matrix[j][i])
-            # print(matrix[i][rows_n - j - 1], end='-')
-            # print(new_matrix[j][i])
-            # matrix[i][rows_n - j - 1] = new_matrix[j][i]
-            j -= 1        
+            new_row.append(tmp_matrix[j][i])
+            j -= 1
         matrix.append(new_row)
         i += 1
