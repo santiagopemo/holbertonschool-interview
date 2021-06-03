201d12050061446a934ffd52015509cab1376b79
santiagopemo <santiagopemo@gmail.com>
2021-06-03 12:27:08 -0500

    corrected betty style

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -3,24 +3,23 @@
 /**
  * find_listint_loop - function that finds the loop in a linked list
  *  @head: linked list head
- * Returns: The address of the node where the loop starts,
+ * Return: The address of the node where the loop starts,
  * or NULL if there is no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *i, *j;
+	listint_t *i, *j;
 
-    for (i = j = head; j && j->next;)
-    {
+	for (i = j = head; j && j->next;)
+	{
 		i = i->next;
 		j = j->next->next;
-
 		if (i == j)
 		{
 			for (i = head; i != j; i = i->next, j = j->next)
 				continue;
 			return (i);
 		}
-    }
-	return(NULL);
+	}
+	return (NULL);
 }
