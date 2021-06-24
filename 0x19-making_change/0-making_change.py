479f9f0b5797f563d5d79cf2b047a1f1b41919a5
santiagopemo <santiagopemo@gmail.com>
2021-06-23 21:25:56 -0500

    fixed total <= 0 condition

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -8,7 +8,7 @@ def makeChange(coins, total):
     """
     Determine the fewest number of coins needed to meet a given amount total
     """
-    if total == 0:
+    if total <= 0:
         return 0
     coins.sort(reverse=True)
     remainig = total
