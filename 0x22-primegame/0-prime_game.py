9c5441658d70ab8a65c4ab14011414598f563a23
santiagopemo <santiagopemo@gmail.com>
2021-08-26 20:03:10 -0500

    added prime_game project

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Prime Game"""
+
+
+def is_prime(num):
+    """Determine if a number is prime"""
+    if num <= 1:
+        return False
+    for i in range(2, int(num / 2) + 1):
+        if num % i == 0:
+            return False
+    else:
+        return True
+
+
+def isWinner(x, nums):
+    """Determine who is the winner of a prime game"""
+    if not nums or x < 1:
+        return None
+    ben_wins = 0
+    maria_wins = 0
+    for i in range(x):
+        arr = [*range(2, nums[i] + 1)]
+        current_player = "Maria"
+        while True:
+            if len(arr) == 0 and current_player == "Maria":
+                ben_wins += 1
+                break
+            if len(arr) == 0 and current_player == "Ben":
+                maria_wins += 1
+                break
+            j = 0
+            has_pick = False
+            addition = 1
+            while j < len(arr):
+                if is_prime(arr[j] and has_pick == False):
+                    has_pick = True
+                    addition = arr[j]
+                if has_pick:
+                    arr.pop(j)
+                j += addition
+            if has_pick:
+                if current_player == "Maria":
+                    current_player = "Ben"
+                else:
+                    current_player = "Maria"
+                continue
+            if not has_pick and current_player == "Maria":
+                ben_wins += 1
+                break
+            if not has_pick and current_player == "Ben":
+                maria_wins += 1
+                break
+    if ben_wins > maria_wins:
+        return "Ben"
+    if ben_wins < maria_wins:
+        return "Maria"
+    return None
---
+++ 0x22-primegame/README.md
@@ -0,0 +1,17 @@
+# 0x22. Prime Game
+## Resources :books:
+* [None]()
+## Requirements :white_check_mark:
+### General
+* Allowed editors: vi, vim, emacs
+* All your files will be interpreted/compiled on Ubuntu 14.04 LTS using python3 (version 3.4.3)
+* All your files should end with a new line
+* The first line of all your files should be exactly #!/usr/bin/python3
+* A README.md file, at the root of the folder of the project, is mandatory
+* Your code should use the PEP 8 style (version 1.7.x)
+* All your files must be executable
+## Tasks
+### Mandatory :page_with_curl:
+- [x] **[0. Prime Game](./0-prime_game.py)** - Maria and Ben are playing a game. Given a set of consecutive integers starting from 1 up to and including n, they take turns choosing a prime number from the set and removing that number and its multiples from the set. The player that cannot make a move loses the game
+## Author :pencil:
+**Santiago Peña Mosquera** - twitter [@santiagopemo](https://twitter.com/santiagopemo) - LinkedIn [Santiago Peña Mosquera](https://www.linkedin.com/in/santiago-pe%C3%B1a-mosquera-abaa20196/)
