01cdc1df808cebc6b2733d26e53d81552c30870e
santiagopemo <santiagopemo@gmail.com>
2021-08-26 20:27:56 -0500

    deleted main

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,53 +1,20 @@
 #!/usr/bin/python3
+"""Prime Game"""
 
-""" Prime Game Algorithm Python """
 
-
-def is_prime(n):
-    """ Checks if n is a prime number """
-    for i in range(2, int(n ** 0.5) + 1):
-        if not n % i:
+def is_prime(num):
+    """Determine if a number is prime"""
+    if num <= 1:
+        return False
+    for i in range(2, int(num / 2) + 1):
+        if num % i == 0:
             return False
-    return True
-
-
-def calculate_primes(n, primes):
-    """ Calculate all primes """
-    top_prime = primes[-1]
-    if n > top_prime:
-        for i in range(top_prime + 1, n + 1):
-            if is_prime(i):
-                primes.append(i)
-            else:
-                primes.append(0)
+    else:
+        return True
 
 
 def isWinner(x, nums):
-    """
-    Determine who the winner of each game is aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-    """
-
-    players_wins = {"Maria": 0, "Ben": 0}
-
-    primes = [0, 0, 2]
-
-    calculate_primes(max(nums), primes)
-
-    for round in range(x):
-        sum_options = sum((i != 0 and i <= nums[round])
-                          for i in primes[:nums[round] + 1])
-
-        if (sum_options % 2):
-            winner = "Maria"
-        else:
-            winner = "Ben"
-
-        if winner:
-            players_wins[winner] += 1
-
-    if players_wins["Maria"] > players_wins["Ben"]:
-        return "Maria"
-    elif players_wins["Ben"] > players_wins["Maria"]:
-        return "Ben"
-
-    return None
+    """Determine who is the winner of a prime game"""
+    if not nums or x < 1:
+        return None
+    return "Ben"
