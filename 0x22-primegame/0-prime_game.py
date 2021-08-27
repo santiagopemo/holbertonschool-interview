06b1f8fcd5fc3c22d011b6cbae7ba16d211bda84
santiagopemo <santiagopemo@gmail.com>
2021-08-26 20:59:48 -0500

    correctet logic

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,61 +1,52 @@
 #!/usr/bin/python3
-"""Prime Game"""
+""" Prime Game """
 
 
-def is_prime(num):
-    """Determine if a number is prime"""
-    if num <= 1:
-        return False
-    for i in range(2, int(num / 2) + 1):
-        if num % i == 0:
+def is_prime(n):
+    """ Checks if n is a prime number """
+    for i in range(2, int(n ** 0.5) + 1):
+        if not n % i:
             return False
-    else:
-        return True
+    return True
 
 
-def play_a_round(arr):
-    """Plays a round of prime game"""
-    j = 0
-    has_pick = False
-    addition = 1
-    while j < len(arr):
-        if is_prime(arr[j]) and not has_pick:
-            has_pick = True
-            addition = arr[j] - 1
-        if has_pick:
-            arr.pop(j)
-        j += addition
-    return has_pick
+def calculate_primes(n, primes):
+    """ Calculate all primes """
+    top_prime = primes[-1]
+    if n > top_prime:
+        for i in range(top_prime + 1, n + 1):
+            if is_prime(i):
+                primes.append(i)
+            else:
+                primes.append(0)
 
 
 def isWinner(x, nums):
-    """Determine who is the winner of a prime game"""
-    if not nums or x < 1:
-        return None
-    ben_wins = 0
-    maria_wins = 0
-    for i in range(x):
-        arr = [*range(2, nums[i] + 1)]
-        current = "Maria"
-        while True:
-            if len(arr) == 0 and current == "Maria":
-                ben_wins += 1
-                break
-            if len(arr) == 0 and current == "Ben":
-                maria_wins += 1
-                break
-            has_pick = play_a_round(arr)
-            if has_pick:
-                current = "Ben" if current == "Maria" else "Maria"
-                continue
-            if not has_pick and current == "Maria":
-                ben_wins += 1
-                break
-            elif not has_pick and current == "Ben":
-                maria_wins += 1
-                break
-    if ben_wins > maria_wins:
-        return "Ben"
-    if ben_wins < maria_wins:
+    """
+    Determine who the winner of each game is.
+    """
+
+    players_wins = {"Maria": 0, "Ben": 0}
+
+    primes = [0, 0, 2]
+
+    calculate_primes(max(nums), primes)
+
+    for round in range(x):
+        sum_options = sum((i != 0 and i <= nums[round])
+                          for i in primes[:nums[round] + 1])
+
+        if (sum_options % 2):
+            winner = "Maria"
+        else:
+            winner = "Ben"
+
+        if winner:
+            players_wins[winner] += 1
+
+    if players_wins["Maria"] > players_wins["Ben"]:
         return "Maria"
+    elif players_wins["Ben"] > players_wins["Maria"]:
+        return "Ben"
+
     return None
